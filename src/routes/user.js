b2edd8c7e14e045f8728d17fea9e9ac3c78134a4
Rob Porter <rob@weeverapps.com>
2020-06-22 16:29:48 -0400

    Try req

--- src/routes/user.js
+++ src/routes/user.js
@@ -83,7 +83,7 @@ module.exports = router => {
         (req, res, next) => {
             console.log('got this far');
             const state = req.params.state;
-            console.log('got a state', req.params);
+            console.log('got a state', req);
             db.Google_Drive_Project_State.findOne({ token: state })
                 .then(state =>
                     findProjectByIdAndOwner([
