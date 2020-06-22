3da4db3e1ce61a47642c178bb4934d68b4192abf
Rob Porter <rob@weeverapps.com>
2020-06-22 16:35:38 -0400

    Fix query

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,7 +15,9 @@ const renderUserPage = (res, name, values = {}) =>
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const THIS_HOST = process.env.HOST ? process.env.HOST : 'http://ecolore-local.org:3001';
+const THIS_HOST = process.env.HOST
+    ? process.env.HOST
+    : 'http://ecolore-local.org:3001';
 
 const findProjectByIdAndOwner = ([id, user]) =>
     db.Project.findOne({
@@ -25,17 +27,17 @@ const findProjectByIdAndOwner = ([id, user]) =>
                 model: db.Membership,
                 where: {
                     userId: user.get('id'),
-                    [Op.or]: [{ role: 'owner' }, { role: 'administrator' }]
-                }
-            }
-        ]
+                    [Op.or]: [{ role: 'owner' }, { role: 'administrator' }],
+                },
+            },
+        ],
     });
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
@@ -44,21 +46,19 @@ passport.use(
         {
             clientID: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
-            callbackURL:
-                THIS_HOST +
-                '/user/auth/google/drive/callback',
-            passReqToCallback: true
+            callbackURL: THIS_HOST + '/user/auth/google/drive/callback',
+            passReqToCallback: true,
         },
-        function(request, accessToken, refreshToken, profile, done) {
+        function (request, accessToken, refreshToken, profile, done) {
             process.nextTick(() => {
                 const state = request.query.state;
 
                 db.Google_Drive_Project_State.findOne({ token: state })
-                    .then(state =>
+                    .then((state) =>
                         db.Google_Drive_OAuth2_Token.create({
                             projectId: state.get('projectId'),
                             accessToken,
-                            refreshToken
+                            refreshToken,
                         })
                     )
                     .then(() => done(null, profile));
@@ -67,27 +67,31 @@ passport.use(
     )
 );
 
-module.exports = router => {
+module.exports = (router) => {
     router.get('/user/auth/google/drive/:state', (req, res, next) =>
         passport.authenticate('google', {
             prompt: 'consent',
             accessType: 'offline',
             scope: ['profile', 'https://www.googleapis.com/auth/drive.file'],
-            state: req.params.state
+            state: req.params.state,
         })(req, res, next)
     );
 
     router.get(
         '/user/auth/google/drive/callback',
-        passwordless.restricted({failureRedirect: '/login' }),
+        passwordless.restricted({ failureRedirect: '/login' }),
         (req, res, next) => {
             const state = req.query.state;
-            db.Google_Drive_Project_State.findOne({ token: state })
-                .then(state =>
+            db.Google_Drive_Project_State.findOne({
+                where: {
+                    token: state,
+                },
+            })
+                .then((state) =>
                     findProjectByIdAndOwner([
                         state.projectId,
-                        res.locals.user
-                    ]).then(project => {
+                        res.locals.user,
+                    ]).then((project) => {
                         console.log('got a project', state, project);
                         return passport.authenticate('google', {
                             successRedirect:
@@ -95,18 +99,18 @@ module.exports = router => {
                             failureRedirect: '/user/auth/google/drive/failure',
                             scope: [
                                 'profile',
-                                'https://www.googleapis.com/auth/drive.file'
-                            ]
+                                'https://www.googleapis.com/auth/drive.file',
+                            ],
                         })(req, res, next);
                     })
                 )
-                .catch(err => res.render('error'));
+                .catch((err) => res.render('error'));
         }
     );
 
     router.get(
         'user/auth/google/failure',
-        passwordless.restricted({failureRedirect: '/login' }),
+        passwordless.restricted({ failureRedirect: '/login' }),
         (req, res) => {
             res.render('google-failure');
         }
@@ -123,22 +127,22 @@ module.exports = router => {
             check('firstName').exists(),
             check('lastName').exists(),
             check('bio').exists(),
-            passwordless.restricted({failureRedirect: '/login' })
+            passwordless.restricted({ failureRedirect: '/login' }),
         ],
-        function(req, res, next) {
+        function (req, res, next) {
             const errors = validationResult(req);
             console.log('errors', errors.mapped());
             // @todo: handle errors better
             if (!errors.isEmpty()) {
-                db.User.findByPk(req.user.id).then(user =>
+                db.User.findByPk(req.user.id).then((user) =>
                     renderUserPage(res, 'registration', { user })
                 );
             } else {
                 const userUpdate = matchedData(req);
                 db.User.update(userUpdate, { where: { id: req.params.id } })
                     .then(() => db.User.findByPk(req.params.id))
-                    .then(user => res.redirect('/profile'))
-                    .catch(err => console.error(err));
+                    .then((user) => res.redirect('/profile'))
+                    .catch((err) => console.error(err));
             }
         }
     );
