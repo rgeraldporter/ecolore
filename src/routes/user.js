e9dd670db81cdaf4c8a929b58a2db0a5a00892b5
Rob Porter <rob@weeverapps.com>
2020-07-08 20:12:21 -0400

    Try something else

--- src/routes/user.js
+++ src/routes/user.js
@@ -97,10 +97,6 @@ module.exports = (router) => {
                             successRedirect:
                                 '/project/' + project.slug + '/edit',
                             failureRedirect: '/user/auth/google/drive/failure',
-                            scope: [
-                                'https://www.googleapis.com/auth/userinfo.profile',
-                                'https://www.googleapis.com/auth/drive.file',
-                            ],
                         })(req, res, next);
                     })
                 )
